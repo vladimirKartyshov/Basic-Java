d8db17a62a91e265f6f221d133afe7a8a449b63f
vladimir Kartyshov <vovan198080@inbox.ru>
2017-06-30 00:19:36 +0300

    Adding new package threads

--- src/main/java/threads/Timer.java
+++ src/main/java/threads/Timer.java
@@ -2,12 +2,22 @@ package threads;
 
 public class Timer implements Runnable{
 
+    private String name;
+
+    public Timer(String name) {
+        this.name = name;
+    }
+
     public void run() {
 
         for(int i=0; i<10; i++){
-            System.out.println(i);
+            System.out.println(name + " : " + i);
 
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-
     }
 }
