5caf7889a7ed663c4267f67ce5a760463371941f
vladimir Kartyshov <vovan198080@inbox.ru>
2017-07-19 23:19:28 +0300

    Adding new package Special

---
+++ src/main/java/special/Special.java
@@ -0,0 +1,11 @@
+package special;
+
+public class Special {
+    public static void main(String[] args) {
+
+        Object object = new Double(1.5);
+        System.out.println(object.getClass());
+
+        Class clazz = object.getClass();// изучение методов object and class
+    }
+}
